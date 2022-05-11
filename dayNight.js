35f7b57f0c4f162b01003ebe776349f593b32c6a
yeobi222 <yeobi222@gmail.com>
2022-05-11 21:22:25 +0900

    11th

---
+++ dayNight.js
@@ -0,0 +1,41 @@
+var Body = {
+  setColor:function (color){
+    document.querySelector('body').style.color = color;
+  },
+  setBackColor:function (color){
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+function changeDN(self) {
+  if(self.value === '2night'){
+    var textColor = "white"
+    Body.setColor(textColor)
+    Body.setBackColor("black")
+    changeStyle('list', textColor);
+    changeStyle('top', textColor);
+    var links = document.querySelectorAll('a');
+    var i = 0;
+    while(i<links.length){
+      links[i].style.color='skyblue';
+      i = i + 1;
+    }
+    self.value = '2day';
+  } else {
+    var textColor = 'black'
+    Body.setColor(textColor)
+    Body.setBackColor("white")
+    changeStyle('list', textColor);
+    changeStyle('top', textColor);
+    var links = document.querySelectorAll('a');
+    var i = 0;
+    while(i<links.length){
+      links[i].style.color='blue';
+      i = i + 1;
+    }
+    self.value = '2night'
+  }
+}
+function changeStyle(idnm, tc){
+  var element = document.getElementById(idnm);
+  element.style.borderColor = tc;
+}
